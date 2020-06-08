1721edc302c22ff283928254dd41b0cc806c8007
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2020-06-08 13:42:42 +0530

    Initial commit

---
+++ app/src/main/java/com/inruca/smartbag/ColorCircle.java
@@ -0,0 +1,246 @@
+package com.inruca.smartbag;
+
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.content.res.ResourcesCompat;
+
+
+public class ColorCircle extends AppCompatImageView {
+
+    private static final int PRESSED_COLOR_LIGHTUP = 255 / 25;
+    private static final int PRESSED_RING_ALPHA = 75;
+    private static final int DEFAULT_PRESSED_RING_WIDTH_DIP = 4;
+    private static final int ANIMATION_TIME_ID = android.R.integer.config_shortAnimTime;
+
+    private int centerY;
+    private int centerX;
+    private int outerRadius;
+    private float notifRadius;
+    private float notifX;
+    private float notifY;
+
+
+    private Paint fillPaint;
+    private Paint focusPaint;
+    private Paint strokePaint;
+    private Paint notificationPaintText;
+    private Paint notificationPaint;
+    private Paint drawOverTextPaint;
+
+    private float animationProgress;
+
+    private int pressedRingWidth;
+    int fillColor;
+    int borderColor;
+
+    private int pressedColor;
+    private ObjectAnimator pressedAnimator;
+    int notificationCount;
+    String drawOverText = "";
+
+    float borderWidth, circleRadius;
+    String desc = " ";
+
+
+    Canvas c;
+
+    public ColorCircle(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public ColorCircle(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public ColorCircle(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+
+        setMeasuredDimension((int) (circleRadius * 2 + borderWidth * 6) | MeasureSpec.EXACTLY, (int) (circleRadius * 2 + borderWidth * 6) | MeasureSpec.EXACTLY);
+    }
+
+    @Override
+    public void setPressed(boolean pressed) {
+        super.setPressed(pressed);
+
+
+        if (pressed) {
+            showPressedRing();
+        } else {
+            hidePressedRing();
+        }
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        this.c = canvas;
+        canvas.drawCircle(centerX, centerY, circleRadius - borderWidth + animationProgress, focusPaint);
+        canvas.drawCircle(centerX, centerY, outerRadius, fillPaint);
+        canvas.drawCircle(centerX, centerY, outerRadius, strokePaint);
+
+        desc = desc.toUpperCase();
+        int xPos = (int) ((getWidth() / 2) - drawOverTextPaint.measureText(desc) / 2);
+        int yPos = (int) ((getHeight() / 2) + centerY * 0.5f);
+
+        canvas.drawText(desc, xPos, yPos, drawOverTextPaint);
+
+        measure((int) (outerRadius * 2 + 2 * borderWidth + 1), (int) (outerRadius * 2 + 2 * borderWidth + 1));
+        notifX = (float) (centerX + (circleRadius * 0.75));
+        notifY = (float) (centerY - (circleRadius * 0.75));
+        notifRadius = (float) (0.25 * outerRadius);
+        if (notificationCount > 0) {
+            canvas.drawCircle((float) notifX, (float) notifY,
+                    (float) notifRadius, notificationPaint);
+            canvas.drawText(notificationCount + "", notifX - (notifRadius / 4), notifY + (notifRadius / 4), notificationPaintText);
+        }
+
+
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        centerX = w / 2;
+        centerY = h / 2;
+        outerRadius = (int) circleRadius;
+
+    }
+
+    public float getAnimationProgress() {
+        return animationProgress;
+    }
+
+    public void setAnimationProgress(float animationProgress) {
+        this.animationProgress = animationProgress;
+        this.invalidate();
+    }
+
+
+    public void setColor(int fillColor, int borderColor) {
+
+
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+        this.pressedColor = getHighlightColor(fillColor, PRESSED_COLOR_LIGHTUP);
+
+        fillPaint.setColor(fillColor);
+        //strokePaint.setShadowLayer(15, 1, 1, Color.BLACK);
+        notificationPaint.setColor(Color.RED);
+        notificationPaintText.setColor(Color.WHITE);
+
+        strokePaint.setColor(borderColor);
+
+        focusPaint.setAlpha(PRESSED_RING_ALPHA);
+
+        this.invalidate();
+    }
+
+    private void hidePressedRing() {
+        fillPaint.setColor(fillColor);
+        pressedAnimator.setFloatValues(pressedRingWidth, 0f);
+        pressedAnimator.start();
+    }
+
+    private void showPressedRing() {
+        fillPaint.setColor(fillPaint.getColor());
+        pressedAnimator.setFloatValues(animationProgress, pressedRingWidth);
+        pressedAnimator.start();
+    }
+
+    public void setNotificationCount(int notificationCount) {
+        this.notificationCount = notificationCount;
+        this.invalidate();
+    }
+
+
+    public int getNotificationCount() {
+        return notificationCount;
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        int color = Color.WHITE;
+        if (attrs != null) {
+            final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ColorCircle);
+            color = a.getColor(R.styleable.ColorCircle_fillColor, color);
+            fillColor = a.getColor(R.styleable.ColorCircle_fillColor, color);
+            desc = (a.getString(R.styleable.ColorCircle_desc) != null ? (a.getString(R.styleable.ColorCircle_desc)) : "");
+            borderColor = a.getColor(R.styleable.ColorCircle_borderColor, getResources().getColor(R.color.inrucaAccent));
+            borderWidth = a.getDimension(R.styleable.ColorCircle_borderWidth, 0);
+            circleRadius = a.getDimension(R.styleable.ColorCircle_circleRadius, centerX);
+            notificationCount = a.getInteger(R.styleable.ColorCircle_notificationCount, 0);
+            pressedRingWidth = (int) borderWidth;
+            a.recycle();
+        }
+
+        this.setFocusable(true);
+        this.setScaleType(ScaleType.CENTER_INSIDE);
+        setClickable(true);
+
+
+        fillPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        fillPaint.setStyle(Paint.Style.FILL);
+
+        notificationPaintText = new Paint(Paint.ANTI_ALIAS_FLAG);
+        notificationPaintText.setStyle(Paint.Style.FILL);
+        int pixel = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                10, getResources().getDisplayMetrics());
+        notificationPaintText.setTextSize(pixel);
+
+
+        drawOverTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        drawOverTextPaint.setStyle(Paint.Style.FILL);
+        drawOverTextPaint.setTextSize(pixel);
+
+
+        drawOverTextPaint.setColor(ResourcesCompat.getColor(getResources(), R.color.inrucaAccent, null));
+        drawOverTextPaint.setTypeface(ResourcesCompat.getFont(context, R.font.lato_black));
+        drawOverTextPaint.setTypeface(Typeface.DEFAULT_BOLD);
+
+
+        notificationPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        notificationPaint.setStyle(Paint.Style.FILL);
+
+        focusPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        focusPaint.setStyle(Paint.Style.STROKE);
+
+        strokePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        strokePaint.setStyle(Paint.Style.STROKE);
+        strokePaint.setStrokeWidth(0);
+
+        pressedRingWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, DEFAULT_PRESSED_RING_WIDTH_DIP, getResources()
+                .getDisplayMetrics());
+
+
+        setColor(fillColor, borderColor);
+
+        focusPaint.setStrokeWidth(0);
+
+        final int pressedAnimationTime = getResources().getInteger(ANIMATION_TIME_ID);
+        pressedAnimator = ObjectAnimator.ofFloat(this, "animationProgress", 0f, 0f);
+        pressedAnimator.setDuration(pressedAnimationTime);
+    }
+
+    private int getHighlightColor(int color, int amount) {
+        return Color.argb(Math.min(255, Color.alpha(color)), Math.min(255, Color.red(color) + amount),
+                Math.min(255, Color.green(color) + amount), Math.min(255, Color.blue(color) + amount));
+    }
+}
---
+++ app/src/main/java/com/inruca/smartbag/SmartBagDashboard.kt
@@ -0,0 +1,149 @@
+package com.inruca.smartbag
+
+import android.os.Bundle
+import android.view.View
+import android.widget.GridLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class SmartBagDashboard : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_dashboard)
+        addQuickAccessCircle()
+    }
+
+    private fun addQuickAccessCircle() {
+        val contactCircle = ColorCircle(this)
+        contactCircle.setColor(
+            android.R.attr.colorBackground,
+            resources.getColor(R.color.inrucaAccent)
+        )
+        contactCircle.id = R.id.oneCircle
+        contactCircle.borderWidth = resources.getDimension(R.dimen.borderWidth)
+        contactCircle.setImageResource(R.drawable.ic_toolbar_button)
+        contactCircle.desc = getString(R.string.quick_access)
+        contactCircle.circleRadius = resources.getDimension(R.dimen.side_menu_quick_access_radius)
+        contactCircle.setOnClickListener(this)
+
+
+        val servicesCircle = ColorCircle(this)
+        servicesCircle.setColor(
+            android.R.attr.colorBackground,
+            resources.getColor(R.color.inrucaAccent)
+        )
+        servicesCircle.id = R.id.twoCircle
+        servicesCircle.borderWidth = resources.getDimension(R.dimen.borderWidth)
+        servicesCircle.setImageResource(R.drawable.ic_toolbar_button)
+        servicesCircle.circleRadius = resources.getDimension(R.dimen.side_menu_quick_access_radius)
+        servicesCircle.setOnClickListener(this)
+        servicesCircle.desc = getString(R.string.quick_access)
+
+        val nmmtStationCircle = ColorCircle(this)
+        nmmtStationCircle.setColor(
+            android.R.attr.colorBackground,
+            resources.getColor(R.color.inrucaAccent)
+        )
+        nmmtStationCircle.id = R.id.threeCircle
+        nmmtStationCircle.borderWidth = resources.getDimension(R.dimen.borderWidth)
+        nmmtStationCircle.setImageResource(R.drawable.ic_toolbar_button)
+        nmmtStationCircle.circleRadius =
+            resources.getDimension(R.dimen.side_menu_quick_access_radius)
+        nmmtStationCircle.setOnClickListener(this)
+        nmmtStationCircle.desc = getString(R.string.quick_access)
+
+
+        val nmmtabCircle = ColorCircle(this)
+        nmmtabCircle.setColor(
+            android.R.attr.colorBackground,
+            resources.getColor(R.color.inrucaAccent)
+        )
+        nmmtabCircle.id = R.id.fourCircle
+        nmmtabCircle.borderWidth = resources.getDimension(R.dimen.borderWidth)
+        nmmtabCircle.setImageResource(R.drawable.ic_toolbar_button)
+        nmmtabCircle.circleRadius = resources.getDimension(R.dimen.side_menu_quick_access_radius)
+        nmmtabCircle.setOnClickListener(this)
+        nmmtabCircle.desc = getString(R.string.quick_access)
+
+
+        val msrtcCircle = ColorCircle(this)
+        msrtcCircle.setColor(
+            android.R.attr.colorBackground,
+            resources.getColor(R.color.inrucaAccent)
+        )
+        msrtcCircle.id = R.id.fiveCircle
+        msrtcCircle.borderWidth = resources.getDimension(R.dimen.borderWidth)
+        msrtcCircle.setImageResource(R.drawable.ic_toolbar_button)
+        msrtcCircle.circleRadius = resources.getDimension(R.dimen.side_menu_quick_access_radius)
+        msrtcCircle.setOnClickListener(this)
+        msrtcCircle.desc = getString(R.string.quick_access)
+
+        val ferryCircle = ColorCircle(this)
+        ferryCircle.setColor(
+            android.R.attr.colorBackground,
+            resources.getColor(R.color.inrucaAccent)
+        )
+        ferryCircle.id = R.id.sixCircle
+        ferryCircle.borderWidth = resources.getDimension(R.dimen.borderWidth)
+        ferryCircle.setImageResource(R.drawable.ic_toolbar_button)
+        ferryCircle.circleRadius = resources.getDimension(R.dimen.side_menu_quick_access_radius)
+        ferryCircle.setOnClickListener(this)
+        ferryCircle.desc = getString(R.string.quick_access)
+
+
+        val newsCircle = ColorCircle(this)
+        newsCircle.setColor(android.R.attr.colorBackground, resources.getColor(R.color.inrucaAccent))
+        newsCircle.id = R.id.sevenCircle
+        newsCircle.borderWidth = resources.getDimension(R.dimen.borderWidth)
+        newsCircle.setImageResource(R.drawable.ic_toolbar_button)
+        newsCircle.circleRadius = resources.getDimension(R.dimen.side_menu_quick_access_radius)
+        newsCircle.setOnClickListener(this)
+        newsCircle.desc = getString(R.string.quick_access)
+
+        val bookmarkCircle = ColorCircle(this)
+        bookmarkCircle.setColor(
+            android.R.attr.colorBackground,
+            resources.getColor(R.color.inrucaAccent)
+        )
+        bookmarkCircle.id = R.id.eightCircle
+        bookmarkCircle.borderWidth = resources.getDimension(R.dimen.borderWidth)
+        bookmarkCircle.setImageResource(R.drawable.ic_toolbar_button)
+        bookmarkCircle.circleRadius = resources.getDimension(R.dimen.side_menu_quick_access_radius)
+        bookmarkCircle.setOnClickListener(this)
+        bookmarkCircle.desc = getString(R.string.quick_access)
+
+
+        val writeCircle = ColorCircle(this)
+        writeCircle.setColor(
+            android.R.attr.colorBackground,
+            resources.getColor(R.color.inrucaAccent)
+        )
+        writeCircle.id = R.id.nineCircle
+        writeCircle.borderWidth = resources.getDimension(R.dimen.borderWidth)
+        writeCircle.setImageResource(R.drawable.ic_toolbar_button)
+        writeCircle.circleRadius = resources.getDimension(R.dimen.side_menu_quick_access_radius)
+        writeCircle.setOnClickListener(this)
+        writeCircle.desc = getString(R.string.quick_access)
+
+
+        
+
+        
+        val grid = findViewById<GridLayout>(R.id.quickAccess) as GridLayout
+        grid.addView(contactCircle)
+        grid.addView(servicesCircle)
+        grid.addView(newsCircle)
+        grid.addView(bookmarkCircle)
+        grid.addView(nmmtStationCircle)
+        grid.addView(nmmtabCircle)
+        grid.addView(msrtcCircle)
+        grid.addView(ferryCircle)
+        grid.addView(writeCircle)
+        
+
+
+    }
+
+    override fun onClick(v: View?) {
+
+    }
+}
